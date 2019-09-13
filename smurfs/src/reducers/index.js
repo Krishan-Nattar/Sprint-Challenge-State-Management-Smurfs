c394a87220756c92289a398c8df8e15f3de85e62
Krishan Nattar <krishan.nattar@gmail.com>
2019-09-13 08:18:46 -0700

    added default reducer

---
+++ smurfs/src/reducers/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const initialState = {
+    smurfs: []
+}
+
+export const reducer = (state=initialState, action) =>{
+    switch(action.type){
+        default: 
+            return state;
+    }
+}
