fa9be742067187b081268a3b22637ca745296610
Krishan Nattar <krishan.nattar@gmail.com>
2019-09-13 08:26:18 -0700

    starting to build get axios call

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -2,6 +2,19 @@ import React from 'react';
 import axios from 'axios';
 
 export const getSmurf = () =>{
+    return dispatch=>{
+        // Start fetching dispatch?
+        axios
+        .get()
+        .then(res=>{
+            console.log(res);
+            // Run dispatch
+        })
+        .catch(err=>{
+            console.log(err);
+            // Error Dispatch?
+        })
+    }
     //something here
 }
 
