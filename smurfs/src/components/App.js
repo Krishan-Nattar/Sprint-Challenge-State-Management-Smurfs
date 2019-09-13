7bd956b625adde9b213ee0a788e0f0f56c2a4630
Krishan Nattar <krishan.nattar@gmail.com>
2019-09-13 08:43:40 -0700

    building form

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import "./App.css";
 import SmurfList from './SmurfList';
+import Form from './Form';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <SmurfList />
+        <Form />
       </div>
     );
   }
---
+++ smurfs/src/components/Form.js
@@ -0,0 +1,35 @@
+import React, {useState} from 'react';
+import {connect} from 'react-redux';
+import {addSmurf} from '../actions';
+
+const FormSmurf = (props) => {
+
+    const [newSmurf, setNewSmurf] = useState({
+        name: "",
+        age: "",
+        height: ""
+    })
+
+    const handleChange = e =>{
+        setNewSmurf({
+            ...newSmurf,
+            [e.target.name] : e.target.value
+        })
+    }
+
+    const handleSubmit = e =>{
+        e.preventDefault();
+        console.log('handleSubming');
+    }
+
+    return ( 
+        <form onSubmit={handleSubmit}>
+            <input name="name" placeholder="Input Smurf Name..." value={newSmurf.name} onChange={handleChange} />
+            <input name="age" placeholder="Input Smurf Age..." value={newSmurf.age} onChange={handleChange} />
+            <input name="height" placeholder="Input Smurf Height..." value={newSmurf.height} onChange={handleChange} />
+            <button type="submit">Add New Smurf</button>
+        </form>
+     );
+}
+ 
+export default connect(null, {addSmurf})(FormSmurf);
